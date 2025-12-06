b6847bd828c185b56358ee70cef3fe99e94d1697
Shikhar Gaur <shikhargaur2008@gmail.com>
2025-12-06 20:00:17 +0530

    savefig updated

--- pyplot/histogram/example.py
+++ pyplot/histogram/example.py
@@ -6,4 +6,6 @@ x = mu + sigma*np.random.randn(10000)
 y = mu + 30*np.random.randn(10000)
 plt.hist([x, y], bins = 100, histtype='barstacked')
 plt.title('Research data Histogram')
+# saving figure
+plt.savefig('.\\pyplot\\histogram\\random.png')
 plt.show()
